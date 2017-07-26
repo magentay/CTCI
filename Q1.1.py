8e9583c44662b495b1179e66fdcb021bab13b2c8
magentay <magentay@ymail.com>
2017-07-26 11:42:45 -0700

    Update Q1.1.py

--- Q1.1.py
+++ Q1.1.py
@@ -1,1 +1,18 @@
 # Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+
+
+# Pythonic solution
+def isUniqueChars(s):
+    return len(set(s)) == len(s)
+
+
+# O(n)
+def isUniqueChars2(s):
+    uchars = set()
+    for c in s:
+        if c in uchars:
+            return False
+        uchars.add(c)        
+    return True
+  
+  
