8fffb837fdd478c28d3f147e27ebf0381504c0ee
magentay <magentay@ymail.com>
2017-07-26 11:48:50 -0700

    Update Q1.1.py

--- Q1.1.py
+++ Q1.1.py
@@ -1,4 +1,5 @@
-# Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+# Implement an algorithm to determine if a string has all unique characters. 
+# What if you can not use additional data structures?
 
 
 # Pythonic solution
