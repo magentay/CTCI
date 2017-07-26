ed5538a407de258e0b67bb9d8b478be075672851
magentay <magentay@ymail.com>
2017-07-26 11:47:43 -0700

    Update Q1.1.py

--- Q1.1.py
+++ Q1.1.py
@@ -3,16 +3,29 @@
 
 # Pythonic solution
 def isUniqueChars(s):
+    if len(s)>256 :
+        return False
     return len(set(s)) == len(s)
 
 
-# O(n)
+# using set O(n)
 def isUniqueChars2(s):
     uchars = set()
     for c in s:
         if c in uchars:
             return False
         uchars.add(c)        
+    return True
+
+#using a bit vector
+def isUniqueCharsBitVector(s):
+    #here as assumption in book, assume string only use lower case 'a' through 'z' 
+    if len(s) > 26: return False
+    checker = 0
+    for char in s:
+        if checker & (1 << ord(char)):
+            return False
+        checker |= (1 << ord(char))
     return True
   
   
