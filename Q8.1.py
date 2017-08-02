ce6dc6358c2d8278f9642201ec0f31dc8b512f78
magentay <magentay@ymail.com>
2017-08-02 13:54:50 -0700

    Create Q8.1.py

---
+++ Q8.1.py
@@ -0,0 +1,26 @@
+# Write a method to generate the nth Fibonacci number.
+
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+        
+        
+ 
+FibArray = [0,1] 
+def fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    elif n<=len(FibArray):
+        return FibArray[n-1]
+    else:
+        temp_fib = fibonacci(n-1)+fibonacci(n-2)
+        FibArray.append(temp_fib)
+        return temp_fib
