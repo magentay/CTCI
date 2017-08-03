d0a179c8fcf2cb811ce198cc2e659298277b1925
magentay <magentay@ymail.com>
2017-08-02 18:19:04 -0700

    Update Q8.3.py

--- Q8.3.py
+++ Q8.3.py
@@ -2,8 +2,19 @@
 
 import itertools
 def findsubsets(S):
-    m = length(S)
-    return set(itertools.combinations(S, m))
+    m = len(S)
+    return [set(j) for i in range(m+1) for j in set(itertools.combinations(S, i))]
     
     
-    
+def subsets(nums):
+    if nums is None: 
+        return None
+    subsets = [[]] 
+    next = [] 
+    for n in nums:
+        for s in subsets:
+            next.append(s + [n])
+        subsets += next
+        next = []
+    return subsets 
+
